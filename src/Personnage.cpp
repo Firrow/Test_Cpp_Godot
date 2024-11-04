5aa81bd2837f4876737bf3d0993a43dad798a5cf
Firrow <firrowmb@gmail.com>
2024-11-05 00:56:49 +0100

    Add Personnage (don't work)

---
+++ src/Personnage.cpp
@@ -0,0 +1,12 @@
+#include "Personnage.h"
+#include <godot_cpp/core/class_db.hpp>
+
+using namespace godot;
+
+// CONSTRUCTEUR
+Personnage::Personnage() {
+}
+
+// DESTRUCTEUR
+Personnage::~Personnage() {
+}
---
+++ src/Personnage.h
@@ -0,0 +1,23 @@
+#ifndef PERSONNAGE_H
+#define PERSONNAGE_H
+
+#include <godot_cpp/classes/sprite2d.hpp>
+
+namespace godot {
+
+	class Personnage {
+	
+	private:
+	
+	protected:
+	
+	public:
+		Personnage();
+		~Personnage();
+	};
+
+}
+
+#endif
+
+//VOIR POUR FAIRE EN SORTE D'AJOUTER UN NOEUD DANS LA HIÉRARCHIE
